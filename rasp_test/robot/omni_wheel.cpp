2d4e954fa5f3311cd6cecc7caf3db28bf55eb7eb
u <u@u-PC>
2016-09-21 16:16:17 +0900

    bug fix 旋回機能 動作未確認

--- rasp_test/robot/omni_wheel.cpp
+++ rasp_test/robot/omni_wheel.cpp
@@ -29,12 +29,12 @@ omni_wheel::~omni_wheel() {}
 
 void omni_wheel::write() {
 	size_t tire_num = config::instance().get<int>("omni_wheel", "tire_num");
-	float p[tire_num];
+	float parallel[tire_num];
 	for (size_t i = 0; i < tire_num; ++i) {
 		float theta = config::instance().get<float>("omni_wheel", "tire_" + std::to_string(i) + "_heading_deg");
 		theta *= M_PI / 180.0f;
 		// タイヤの向きベクトルと速度ベクトルの内積
-		p[i] = _velocity_x * cos(theta) + _velocity_y * sin(theta);
+		parallel[i] = _velocity_x * cos(theta) + _velocity_y * sin(theta);
 	}
 	// ロボットの角度制御
 	_target_heading_rad += _angular_velocity;
@@ -43,7 +43,11 @@ void omni_wheel::write() {
 	float mv = pid_manager<float>::instance().get("omni_wheel_heading").update(e);
 
 	for (size_t i = 0; i < tire_num; ++i) {
-		dc_motor::instance().set("wheel" + std::to_string(i), p[i] + mv);
+		float tire_position_deg = config::instance().get<float>("omni_wheel", "tire_" + std::to_string(i) + "_position_deg");
+		float tire_heading_deg = config::instance().get<float>("omni_wheel", "tire_" + std::to_string(i) + "_heading_deg");
+		float between_deg = tire_position_deg + 90.0f - tire_heading_deg;
+		float turn = mv * cos(between_deg * M_PI / 180.0f);
+		dc_motor::instance().set("wheel" + std::to_string(i), parallel[i] + turn);
 	}
 }
 
